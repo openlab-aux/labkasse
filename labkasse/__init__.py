6958d258f707ee394c30d4a38e4b686d46ef0dea
Johannes Fürmann <johannes@weltraumpflege.org>
2014-04-03 21:24:02 +0200

    integrate swagger #yolo

--- labkasse/__init__.py
+++ labkasse/__init__.py
@@ -3,6 +3,7 @@ from flask_peewee.db import Database
 from flask_admin import Admin
 from flask_admin.contrib.peewee import ModelView
 from flask.ext.restful import Resource, Api
+from flask_restful_swagger import swagger
 
 app = Flask(__name__)
 
@@ -18,5 +19,6 @@ admin.add_view(ModelView(Item))
 admin.add_view(ModelView(Donation))
 
 api = Api(app)
+api = swagger.docs(api, api_spec_url='/api-docs')
 
 import labkasse.api
